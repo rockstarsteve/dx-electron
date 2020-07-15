97d3d35f4447bf6fcab0d3be418a67c01517741b
rockstaryao <670139644@qq.com>
2020-07-15 09:28:59 +0800

    添加桌面项目

---
+++ counter/src/renderer/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'counter'
+  }
+</script>
+
+<style>
+  #app {
+    height: 100vh;
+    width: 100%;
+  }
+  *::-webkit-scrollbar {
+    display: none;
+  }
+  * {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  button{
+    outline: none;
+  }
+
+</style>
---
+++ counter/src/renderer/views/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <button @click="login">到登录页面</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    methods: {
+      login () {
+        console.log(`tologin。。。`)
+        this.$electron.ipcRenderer.send('switchToLogin')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
